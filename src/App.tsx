64a7369f1da3ed744b73eada95f86d98d7e27747
Júlia Bueno Seganfredo <150600044+jb-seganfredo@users.noreply.github.com>
2024-09-10 19:18:44 -0300

    feat(header): adiciona menu no header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import Routes from "./routes/routes";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="h-screen w-full">
+      <Header />
+
       <Routes />
     </div>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,79 @@
-export default function Header() {
-  return <header>Header</header>;
+import logo from "../assets/logo.png";
+import menuIcon from "../assets/menuIcon.png";
+import { useState } from "react";
+
+function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <div>
+      <header className="flex items-center justify-between px-8 py-6 text-sky-950 md:px-32">
+        <a href="#">
+          <img
+            src={logo}
+            alt="brand logo"
+            className="w-25 transition-all hover:scale-105"
+          />
+        </a>
+
+        <ul className="hidden items-center gap-12 text-base font-semibold xl:flex">
+          <li className="cursor-pointer rounded-md p-3 transition-all hover:bg-sky-900 hover:text-white">
+            Top Offers
+          </li>
+          <li className="cursor-pointer rounded-md p-3 transition-all hover:bg-sky-900 hover:text-white">
+            About Us
+          </li>
+          <li className="cursor-pointer rounded-md p-3 transition-all hover:bg-sky-900 hover:text-white">
+            Our Team
+          </li>
+          <li className="cursor-pointer rounded-md p-3 transition-all hover:bg-sky-900 hover:text-white">
+            References
+          </li>
+        </ul>
+
+        <div className="relative hidden items-center justify-center gap-4 font-semibold md:flex">
+          <a
+            href="#"
+            className="rounded-md bg-sky-900 p-3 text-white transition-all hover:scale-105 hover:opacity-70"
+          >
+            Contact Us
+          </a>
+        </div>
+
+        <img
+          src={menuIcon}
+          alt="menu icon"
+          className="bx bx-menu block w-12 cursor-pointer xl:hidden"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        />
+
+        <div
+          className={`absolute left-0 top-24 flex w-full transform flex-col items-center bg-white font-bold transition-transform xl:hidden ${isMenuOpen ? "zpacity-100" : "opacity-0"}`}
+          style={{ transition: "transform 0.3s ease" }}
+        >
+          <li className="w-full cursor-pointer list-none p-3 text-center font-semibold text-sky-950 transition-all hover:bg-sky-900 hover:text-white">
+            Top Sellers
+          </li>
+
+          <li className="w-full cursor-pointer list-none p-3 text-center font-semibold text-sky-950 transition-all hover:bg-sky-900 hover:text-white">
+            About Us
+          </li>
+
+          <li className="w-full cursor-pointer list-none p-3 text-center font-semibold hover:bg-sky-900 hover:text-white">
+            Our Team
+          </li>
+
+          <li className="w-full cursor-pointer list-none p-3 text-center font-semibold text-sky-950 transition-all hover:bg-sky-900 hover:text-white">
+            References
+          </li>
+
+          <li className="w-full cursor-pointer list-none bg-sky-900 p-3 text-center font-semibold text-white transition-all hover:opacity-80 md:hidden">
+            Contact Us
+          </li>
+        </div>
+      </header>
+    </div>
+  );
 }
+
+export default Header;
