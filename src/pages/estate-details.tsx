fca22d2cf1e62fc630a6e2c514a94bc5127500ee
AlexandreDresch <connectWithAlexandre@proton.me>
2024-09-14 23:29:21 -0300

    style: align details to image slider

--- src/pages/estate-details.tsx
+++ src/pages/estate-details.tsx
@@ -15,7 +15,7 @@ export default function EstateDetails() {
   return (
     <>
       <Header />
-      <div className="flex min-w-full max-w-7xl justify-center gap-20 px-5 py-10 sm:px-10 md:px-20 lg:px-40 lg:py-20">
+      <div className="flex min-w-full max-w-7xl justify-center gap-10 px-5 py-10 sm:px-10 md:px-20 lg:px-40 lg:py-20">
         <main className="flex w-full flex-col space-y-10 lg:w-2/3">
           <h1 className="font-merriweather text-lg font-bold md:text-2xl lg:text-3xl">
             {estateData.title}
@@ -23,99 +23,104 @@ export default function EstateDetails() {
 
           <ImageSlider name={estateData.title} imageUrls={estateData.images} />
 
-          <section className="w-full max-w-3xl space-y-5 lg:space-y-10">
-            <div className="flex w-full justify-between">
-              <Badge
-                icon="./icons/company.svg"
-                description={translatePropertyType(estateData.propertyType)}
-              />
+          <div className="flex max-w-3xl flex-col items-center space-y-10">
+            <section className="w-full max-w-2xl space-y-5 lg:space-y-10">
+              <div className="flex w-full justify-between">
+                <Badge
+                  icon="./icons/company.svg"
+                  description={translatePropertyType(estateData.propertyType)}
+                />
 
-              <Badge
-                icon="./icons/dimensions.svg"
-                description={`${estateData.size}m²`}
-              />
+                <Badge
+                  icon="./icons/dimensions.svg"
+                  description={`${estateData.size}m²`}
+                />
 
-              <Badge icon="./icons/pin.svg" description={estateData.location} />
-            </div>
-
-            <div className="flex justify-between rounded-md bg-secondary p-2 text-sm sm:p-4 md:text-base">
-              <div className="font-opensans">
-                <p>
-                  {estateData.type === "rent" ? "Aluguel " : "Preço "}partindo
-                  de:
-                </p>
-                <p className="font-bold">
-                  {formatToBRL(estateData.price)}
-                  {estateData.type === "rent" && "/mês"}
-                </p>
+                <Badge
+                  icon="./icons/pin.svg"
+                  description={estateData.location}
+                />
               </div>
 
-              <button className="rounded-md bg-primary px-2 font-bold text-white transition-opacity hover:bg-primary/95 sm:px-4">
-                <span>Tenho interesse</span>
-              </button>
-            </div>
+              <div className="flex justify-between rounded-md bg-secondary p-2 text-sm sm:p-4 md:text-base">
+                <div className="font-opensans">
+                  <p>
+                    {estateData.type === "rent" ? "Aluguel " : "Preço "}partindo
+                    de:
+                  </p>
+                  <p className="font-bold">
+                    {formatToBRL(estateData.price)}
+                    {estateData.type === "rent" && "/mês"}
+                  </p>
+                </div>
 
-            <ContentContainer content={estateData.briefDescription} />
-
-            <ContentContainer
-              title="Características:"
-              content={estateData.amenities}
-            />
-
-            <ContentContainer
-              title="Detalhes do Layout:"
-              content={estateData.layoutDetails}
-            />
-
-            <ContentContainer
-              title="Detalhes da Mobília:"
-              content={estateData.furnitureDetails}
-            />
-
-            <ContentContainer
-              title="Localização:"
-              content={estateData.locationDetails}
-            />
-
-            <ContentContainer
-              title="Opinião da Imobiliária:"
-              content={estateData.propertyOpinion}
-            />
-          </section>
-
-          <section className="flex max-w-3xl">
-            <Map
-              position={estateData.map}
-              title={estateData.title}
-              details={estateData.amenities}
-            />
-          </section>
-
-          <section className="max-w-3xl space-y-5">
-            <h3 className="font-merriweather text-lg font-bold">
-              Você pode se interessar em
-            </h3>
-
-            <div className="md:[&::-webkit-scrollbar-thumb]:bg-accent flex h-full w-full max-w-3xl gap-4 overflow-x-auto py-2 md:overflow-x-scroll md:[&::-webkit-scrollbar-thumb]:rounded-lg [&::-webkit-scrollbar]:invisible">
-              {otherServices.map((service) => (
-                <button
-                  key={service.id}
-                  className="relative flex h-[100px] max-h-[100px] w-[150px] min-w-[150px] items-center justify-center md:max-h-[180px] md:w-[200px]"
-                >
-                  <img
-                    src={service.image}
-                    alt={service.title}
-                    className="size-auto max-h-[100px] min-w-[150px] rounded-sm object-cover md:w-[200px]"
-                  />
-                  <span className="absolute bottom-0 rounded-sm bg-black/10 px-1 font-bold text-white">
-                    {service.title}
-                  </span>
+                <button className="rounded-md bg-primary px-2 font-bold text-white transition-opacity hover:bg-primary/95 sm:px-4">
+                  <span>Tenho interesse</span>
                 </button>
-              ))}
-            </div>
-          </section>
+              </div>
+
+              <ContentContainer content={estateData.briefDescription} />
+
+              <ContentContainer
+                title="Características:"
+                content={estateData.amenities}
+              />
+
+              <ContentContainer
+                title="Detalhes do Layout:"
+                content={estateData.layoutDetails}
+              />
+
+              <ContentContainer
+                title="Detalhes da Mobília:"
+                content={estateData.furnitureDetails}
+              />
+
+              <ContentContainer
+                title="Localização:"
+                content={estateData.locationDetails}
+              />
+
+              <ContentContainer
+                title="Opinião da Imobiliária:"
+                content={estateData.propertyOpinion}
+              />
+            </section>
+
+            <section className="flex w-full max-w-2xl">
+              <Map
+                position={estateData.map}
+                title={estateData.title}
+                details={estateData.amenities}
+              />
+            </section>
+
+            <section className="max-w-2xl space-y-5">
+              <h3 className="font-merriweather text-lg font-bold">
+                Você pode se interessar em
+              </h3>
+
+              <div className="md:[&::-webkit-scrollbar-thumb]:bg-accent flex h-full w-full max-w-3xl gap-4 overflow-x-auto py-2 md:overflow-x-scroll md:[&::-webkit-scrollbar-thumb]:rounded-lg [&::-webkit-scrollbar]:invisible">
+                {otherServices.map((service) => (
+                  <button
+                    key={service.id}
+                    className="relative flex h-[100px] max-h-[100px] w-[150px] min-w-[150px] items-center justify-center md:max-h-[180px] md:w-[200px]"
+                  >
+                    <img
+                      src={service.image}
+                      alt={service.title}
+                      className="size-auto max-h-[100px] min-w-[150px] rounded-sm object-cover md:w-[200px]"
+                    />
+                    <span className="absolute bottom-0 rounded-sm bg-black/10 px-1 font-bold text-white">
+                      {service.title}
+                    </span>
+                  </button>
+                ))}
+              </div>
+            </section>
+          </div>
         </main>
-        <aside className="sticky top-10 hidden h-max w-[352px] flex-col space-y-10 lg:flex">
+        <aside className="hidden h-max w-[352px] flex-col space-y-10 pt-16 lg:flex">
           <form action="" className="space-y-2 rounded-sm bg-secondary p-4">
             <h3 className="font-merriweather text-xl font-bold">
               Entre em contato
