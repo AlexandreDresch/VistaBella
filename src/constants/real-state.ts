2f814b868dcc018f38b874bd64c4d1e6b6b09480
AlexandreDresch <connectWithAlexandre@proton.me>
2024-09-23 23:02:19 -0300

    fix: remove redundances in otherServices

--- src/constants/real-state.ts
+++ src/constants/real-state.ts
@@ -474,7 +474,7 @@ export const otherServices = [
   },
   {
     id: 2,
-    title: "Ofertas Imobiliárias",
+    title: "Ofertas",
     image:
       "https://images.unsplash.com/photo-1672082497059-1e6c0209eeeb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
